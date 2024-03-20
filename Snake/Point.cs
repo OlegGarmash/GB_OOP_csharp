d307456568e662d1178f81a577e072ad900520cc
OlegGarmash <oleg@stavrol.ru>
2024-03-20 17:10:12 +0400

    lesson3. Классы и объекты

---
+++ Snake/Point.cs
@@ -0,0 +1,12 @@
+class Point
+{
+    public int x;
+    public int y;
+    public char sym;
+
+    public void Draw()
+    {
+        Console.SetCursorPosition(x, y);
+        System.Console.WriteLine(sym);
+    }
+}
